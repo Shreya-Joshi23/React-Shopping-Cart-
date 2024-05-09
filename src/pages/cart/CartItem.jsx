e5623ce99ea6e7904a14f68dd8ba1642470cc9d5
Shreya-Joshi23 <joshi2003shreya@gmail.com>
2024-05-09 17:37:41 +0530

    functionality to update count

--- src/pages/cart/CartItem.jsx
+++ src/pages/cart/CartItem.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Cart.css";
+import { ShopContext } from "../../context/shop-context";
 
 const CartItem = ({ item }) => {
+
+  const {cartItems,updatecartitemcount,addtocart,removefromcart}=useContext(ShopContext);
   console.log({ item });
 
   return (
@@ -9,7 +12,17 @@ const CartItem = ({ item }) => {
       <div className="item-image">
         <img src={item.image} alt="image1"></img>
       </div>
+
+      <div>
+      <p className="item-title">{item.title}</p>
+      
       <p className="item-price">Price: ${item.price}</p>
+      <div className="countHandler">
+        <button onClick={()=>addtocart(item.id)}>+</button>
+        <input value={cartItems[item.id]} onChange={(e)=>updatecartitemcount(Number(e.target.value),item.id)}/>
+        <button onClick={()=>removefromcart(item.id)}>-</button>
+      </div>
+      </div>
     </div>
   );
 };
